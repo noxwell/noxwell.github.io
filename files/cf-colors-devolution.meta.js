872e15886a43274952b689c675d2542654adf9ae
Alexey Vetrov <vetaleha@gmail.com>
2015-10-02 18:36:44 +0600

    Added files section

---
+++ files/cf-colors-devolution.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id             cf-colors-devolution
+// @name           CF de-Revolution of Colors
+// @version        0.1
+// @namespace      https://github.com/noxwell/cf-colors-devolution
+// @updateURL      https://noxwell.github.io/files/cf-colors-devolution.meta.js
+// @downloadURL    https://noxwell.github.io/files/cf-colors-devolution.user.js
+// @description    This userscript adds ability to view user's profiles in CodeForces as it was before Revolution of Colors.
+// @include        https://codeforces.com/profile/*
+// @include        http://codeforces.com/profile/*
+// @match          https://codeforces.com/profile/*
+// @match          http://codeforces.com/profile/*
+// @grant          none
+// ==/UserScript==
